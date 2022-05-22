f3843cc2a3df8ba05c647d4e3f6baa9b5eddb28f
EivydasV <eivydasvickus@gmail.com>
2022-05-22 15:39:19 +0300

    first commit

---
+++ app/Http/Controllers/EventUsers.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\Accept;
+use App\Models\EventUsers as ModelsEventUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class EventUsers extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except('registerEvent');
+    }
+
+    public function registerEvent(Request $request)
+    {
+
+        $validated = $request->validate([
+            'event_id' => 'required|numeric',
+            'name' => 'required',
+            'email' => 'required|email',
+            'telephone' => 'string',
+        ]);
+
+        $check = ModelsEventUsers::where('event_id', $validated['event_id'])
+            ->where('email', $validated['email'])->first();
+
+        if ($check === null) {
+            ModelsEventUsers::create($validated);
+            return redirect('/');
+        }
+
+        return redirect()->back();
+    }
+
+    public function accept($id, $condition)
+    {
+
+        $user = ModelsEventUsers::find($id);
+
+        Mail::to($user->email)->send(new Accept($condition));
+
+        $user->delete();
+
+        return redirect()->back();
+    }
+}
---
+++ resources/views/events/event.blade.php
@@ -0,0 +1,50 @@
+@extends('main')
+
+@section('content')
+    <h1>
+        {{$event->title}}
+    </h1>
+
+    <div class="container">
+        <form action="/register/event" method="POST">
+            @csrf
+            <input type="hidden" id="event_id" name="event_id" value="{{$event->id}}">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input name="name" id="name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="telephone">Telephone</label>
+                <input type="text" id="telephone" name="telephone" class="form-control">
+            </div>
+            <div class="form-group mt-2">
+                <button class="btn btn-primary" type="submit">Request</button>
+            </div>
+        </form>
+    </div>
+
+    @if ($show)
+        <div>
+            @foreach ($event->eventUser as $user)
+                <div class="border border-dark" style="width: 25vh">
+                    <div class="p-2">
+                        {{$user->name}}
+                        <a href="/accept/event/{{$user->id}}/accept" class="btn btn-primary">Accept</a>
+                        <a href="/accept/event/{{$user->id}}/decline" class="btn btn-primary">Decline</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <form action="/delete/event/{{$event->id}}" method="POST">
+            @csrf
+            <button type="submit" class="btn btn-danger">add</button>
+        </form>
+    @endif
+
+
+
+@endsection
